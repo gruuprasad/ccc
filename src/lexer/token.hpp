76ddc0f972f852720c47aebd9b8ef1d2c76fcfab
guruhegde <s8guhegd.stud.uni-saarland.de>
2018-10-30 14:20:27 +0100

    Implement TokenView class
    This class stores pointer to start of token string and
    length of token string. This helps us avoid creating
    std::string object. This improves overall memory usage.
    
    I hope we gain in terms of speed also, although I didn't a large
    improvement in speed for our tests.

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -5,20 +5,29 @@
 #include <ostream>
 #include "token_type.hpp"
 
+struct TokenView {
+  const char *first;
+  unsigned int length;
+  TokenView() : first(nullptr), length(0) {}
+  TokenView(const char * first, unsigned int length) : first(first), length(length) {}
+  bool empty() const { return first == nullptr || length == 0; }
+  friend inline std::ostream &operator<<(std::ostream &os, const TokenView &tv) {
+    os.write(tv.first, tv.length);
+    return os;
+  }
+};
+
 class Token {
 public:
-  Token(TokenType type, unsigned long line, unsigned long column, std::string extra = std::string());
   TokenType getType() const;
   unsigned long getLine() const;
   unsigned long getColumn() const;
-  const std::string &getExtra() const;
   const std::string name() const;
   const std::string token_type() const;
 private:
   const TokenType type;
   const unsigned long line;
   const unsigned long column;
-  std::string extra;
 public:
   friend std::ostream &operator<<(std::ostream &os, const Token &token);
 };
