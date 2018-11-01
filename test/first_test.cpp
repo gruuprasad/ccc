10eadcc540d19024a707801573155fc4c3ef12db
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2018-11-01 20:03:19 +0100

    resolved merge conflict

--- test/first_test.cpp
+++ test/first_test.cpp
@@ -1,6 +1,5 @@
 #include "catch.hpp"
 #include "../src/entry/entry_point_handler.hpp"
-#include "../src/ast/statement.hpp"
 
 TEST_CASE("Our compiler should fail for now.") {
   REQUIRE(1 == EntryPointHandler().handle(0, nullptr));
@@ -16,59 +15,22 @@ TEST_CASE("Our tokenizer should work.") {
   REQUIRE(0 == EntryPointHandler().handle(3, args));
 }
 
-TEST_CASE("Our tokenizer should fail on invalid.") {
+TEST_CASE("Our parser should work.") {
   char *args[] = {
       (char*)"c4",
-      (char*)"--tokenize",
-      (char*)"../examples/error.c",
+      (char*)"--parse",
+      (char*)"../examples/test.c",
       nullptr
   };
+  REQUIRE(0 == EntryPointHandler().handle(3, args));
+}
+
+TEST_CASE("Our tokenizer should fail on invalid.") {
+  char *args[] = {(char *) "c4", (char *) "--tokenize", (char *) "../examples/error.c", nullptr};
   REQUIRE(1 == EntryPointHandler().handle(3, args));
 }
 
-TEST_CASE("statement run") {
-  int count = 0;
-  std::vector<ASTNode *> scope;
-
-  scope.emplace_back(new IfStatement(count++,
-      new ConstantExpression(count++, new IntegerConstant(count++, 8)),
-      new ExpressionStatement(count++,
-          new AdditiveExpression(count++,
-              new ConstantExpression(count++, new CharacterConstant(count++, 'a')),
-              new Identifier(count++)))));
-  scope.emplace_back(new WhileStatement(count++,
-      new Identifier(count++),
-      new ExpressionStatement(count++, new Identifier(count++))));
-  scope.emplace_back(new IfElseStatement(count++,
-      new Identifier(count++),
-      new IfElseStatement(count++,
-          new EqualityExpression(count++,
-              new ConstantExpression(count++, new IntegerConstant(count++, 42)),
-              new ConstantExpression(count++, new IntegerConstant(count++, 3))),
-          new ExpressionStatement(count++,
-              new ConditionalExpression(count++,
-                  new RelationalExpression(count++,
-                      new ConstantExpression(count++, new IntegerConstant(count++, 1)),
-                      new ConstantExpression(count++, new IntegerConstant(count++, 2))),
-                  new MultiplicativeExpression(count++,
-                      new ConstantExpression(count++, new IntegerConstant(count++, -1)),
-                      new ConstantExpression(count++, new IntegerConstant(count++, 256))),
-                  new ConstantExpression(count++, new CharacterConstant(count++, 'Q')))),
-          new ExpressionStatement(count++,
-              new AssignmentExpression(count++,
-                  new Identifier(count++),
-                  new ConstantExpression(count++, new CharacterConstant(count++, 'u'))))),
-      new ExpressionStatement(count++, new Identifier(count++))));
-  scope.emplace_back(new ReturnStatement(count++, new Identifier(count++)));
-
-  ASTNode *root = new CompoundStatement(count, scope);
-  std::ofstream file;
-  file.open("ast.gv");
-  file << root->toGraph();
-  file.close();
-
-//  std::cout << root->toGraph();
-
-  delete root;
+TEST_CASE("Our parser should fail on invalid.") {
+  char *args[] = {(char *) "c4", (char *) "--parse", (char *) "../examples/error.c", nullptr};
+  REQUIRE(1 == EntryPointHandler().handle(3, args));
 }
-
