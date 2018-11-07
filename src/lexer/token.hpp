3d1f092406dda42fa7bd942bf32ab8c4cb7fc8f0
guruhegde <s8guhegd.stud.uni-saarland.de>
2018-11-07 19:05:00 +0100

    Token: Implement special member functions

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -9,8 +9,15 @@ namespace ccc {
 
 class Token {
 public:
+  Token() {}
   Token(ccc::TokenType type, unsigned long line, unsigned long column, std::string extra = std::string());
+  Token(const Token & t) = default;
+  Token& operator=(const Token & t) = default;
+  Token(Token && t) = default;
+  Token& operator=(Token && t) = default;
+  ~Token() = default;
   ccc::TokenType getType() const;
+
   unsigned long getLine() const;
   unsigned long getColumn() const;
   const std::string &getExtra() const;
@@ -19,9 +26,9 @@ public:
   bool is(TokenType expected) const { return type == expected; }
 
 private:
-  const ccc::TokenType type;
-  const unsigned long line;
-  const unsigned long column;
+  ccc::TokenType type;
+  unsigned long line;
+  unsigned long column;
   std::string extra;
 public:
   friend std::ostream &operator<<(std::ostream &os, const Token &token);
