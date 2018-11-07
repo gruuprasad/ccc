c294d59eb4087a3ed663d953ac52109455b052fa
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2018-11-07 13:48:39 +0100

    runtime visualization

---
+++ scripts/runtime.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python3 stats.py ../build/release/c4
---
+++ scripts/stats.py
@@ -0,0 +1,48 @@
+import sys
+import matplotlib.pyplot as plt
+import os
+from timeit import default_timer as timer
+from tqdm import tqdm
+
+int = []
+val = dict()
+
+t = [timer()]
+
+i = 1
+
+files = []
+
+exe = sys.argv[1:]
+
+print("gernating input files...")
+
+for i in tqdm(range(25)):
+    size = i * 50000
+    if not os.path.exists("./sample"):
+        os.makedirs("./sample")
+    file = open("./sample/test" + str(size) + ".c", "a+")
+    while os.path.getsize("./sample/test" + str(size) + ".c") < size:
+        file.write("int a = 0;\n")
+    file.close()
+    files += ["./sample/test" + str(size) + ".c"]
+    int += [size / 1000]
+
+for e in exe:
+    val[e] = []
+
+print("lexing...")
+for f in tqdm(files):
+    for e in exe:
+        start = timer()
+        os.system("./" + e + " --tokenize " + f + " > /dev/null")
+        val[e] += [(timer() - start)]
+
+plt.figure(figsize=(15, 5))
+
+for e in exe:
+    plt.plot(int, val[e], "-o", label=e)
+plt.legend()
+plt.xlabel("Input size [kByte]")
+plt.ylabel('Runtime [seconds]')
+plt.savefig("stat.svg", dpi=100)
