f49fbfe82ca74686e5c32bcbbc838edb492fe8cb
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2018-12-18 22:24:44 +0100

    allow_failure

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -24,27 +24,27 @@ template <typename T> void print(const T &t) {
     REQUIRE(fp.fail() == false);                                               \
   }
 
-
-#define DECLARATION_TESTS(type) \
-TEST_CASE("Fast Parser:empty declaration test "#type) { \
-  PARSE_VALID(type" ;") \
-  PARSE_VALID(type" a;")\
-  PARSE_VALID(type" (a);")\
-  PARSE_VALID(type" * p;")\
-  PARSE_VALID(type" ** p;")\
-  PARSE_VALID(type" ** (p);")\
-  PARSE_VALID(type" ***** (p);")\
-  PARSE_VALID(type" a (" + std::string(type) + " a);")\
-  PARSE_VALID(type" ;" + std::string(type) + " ;") \
-  PARSE_VALID(type" a;" + std::string(type) + " a;")\
-  PARSE_VALID(type" (a);"+ std::string(type) + " (a);")\
-  PARSE_VALID(type" * p;" + std::string(type) + " * p;")\
-  PARSE_VALID(type" ** p;" + std::string(type) + " ** p;")\
-  PARSE_VALID(type" ** (p);" + std::string(type) + " ** (p);")\
-  PARSE_VALID(type" ***** (p);" + std::string(type) + " ***** (p);")\
-  PARSE_VALID("void a; char b; short c; int d; struct e;")\
-  PARSE_VALID(type" a (" + std::string(type) + " a, void b, char c, short e);")\
-}
+#define DECLARATION_TESTS(type)                                                \
+  TEST_CASE("Fast Parser:empty declaration test " #type) {                     \
+    PARSE_VALID(type " ;")                                                     \
+    PARSE_VALID(type " a;")                                                    \
+    PARSE_VALID(type " (a);")                                                  \
+    PARSE_VALID(type " * p;")                                                  \
+    PARSE_VALID(type " ** p;")                                                 \
+    PARSE_VALID(type " ** (p);")                                               \
+    PARSE_VALID(type " ***** (p);")                                            \
+    PARSE_VALID(type " a (" + std::string(type) + " a);")                      \
+    PARSE_VALID(type " ;" + std::string(type) + " ;")                          \
+    PARSE_VALID(type " a;" + std::string(type) + " a;")                        \
+    PARSE_VALID(type " (a);" + std::string(type) + " (a);")                    \
+    PARSE_VALID(type " * p;" + std::string(type) + " * p;")                    \
+    PARSE_VALID(type " ** p;" + std::string(type) + " ** p;")                  \
+    PARSE_VALID(type " ** (p);" + std::string(type) + " ** (p);")              \
+    PARSE_VALID(type " ***** (p);" + std::string(type) + " ***** (p);")        \
+    PARSE_VALID("void a; char b; short c; int d; struct e;")                   \
+    PARSE_VALID(type " a (" + std::string(type) +                              \
+                " a, void b, char c, short e);")                               \
+  }
 
 TEST_CASE("Fast Parser: Struct declaration test"){
     PARSE_VALID("struct { int; };") PARSE_VALID(
@@ -87,12 +87,12 @@ TEST_CASE("Fast Parser: Struct declaration test"){
 DECLARATION_TESTS("void") DECLARATION_TESTS("char") DECLARATION_TESTS("short")
     DECLARATION_TESTS("int") DECLARATION_TESTS("struct A")
 
-#define PARSE_VALID_EXPRESSION(language) \
-  {\
-    auto token_list = FastLexer(language).lex(); \
-    auto fp = FastParser(token_list); \
-    fp.parse(PARSE_TYPE::EXPRESSION); \
-    REQUIRE(fp.fail() == false); \
+#define PARSE_VALID_EXPRESSION(language)                                       \
+  {                                                                            \
+    auto token_list = FastLexer(language).lex();                               \
+    auto fp = FastParser(token_list);                                          \
+    fp.parse(PARSE_TYPE::EXPRESSION);                                          \
+    REQUIRE(fp.fail() == false);                                               \
   }
 
     // Test simple expressions
@@ -131,55 +131,52 @@ TEST_CASE("Fast Parser:binary expression test"){
 TEST_CASE("debug") {
 }
 
-#define PARSE_VALID_STATEMENT(language) \
-  {\
-    auto token_list = FastLexer(language).lex(); \
-    auto fp = FastParser(token_list); \
-    fp.parse(PARSE_TYPE::STATEMENT); \
-    REQUIRE(fp.fail() == false); \
-  }\
-
-#define PARSE_INVALID_STATEMENT(language) \
-  {\
-    auto token_list = FastLexer(language).lex(); \
-    auto fp = FastParser(token_list); \
-    fp.parse(PARSE_TYPE::STATEMENT); \
-    REQUIRE(fp.fail() == true); \
+#define PARSE_VALID_STATEMENT(language)                                        \
+  {                                                                            \
+    auto token_list = FastLexer(language).lex();                               \
+    auto fp = FastParser(token_list);                                          \
+    fp.parse(PARSE_TYPE::STATEMENT);                                           \
+    REQUIRE(fp.fail() == false);                                               \
   }
 
-TEST_CASE("Fast Parser: simple statement test") {
-  PARSE_VALID_STATEMENT("{ 1;}") //expression-statement
-  PARSE_VALID_STATEMENT("{ a: 1;}") // labeled-statement
-  PARSE_VALID_STATEMENT("{ int a;}") // declaration in compound statement
-  PARSE_VALID_STATEMENT("{}")
-  PARSE_INVALID_STATEMENT("{ 1;") 
-  PARSE_INVALID_STATEMENT("{ a: 1;") 
-  PARSE_INVALID_STATEMENT("{ int a;")
-  PARSE_INVALID_STATEMENT("{ ")
-}
+#define PARSE_INVALID_STATEMENT(language)                                      \
+  {                                                                            \
+    auto token_list = FastLexer(language).lex();                               \
+    auto fp = FastParser(token_list);                                          \
+    fp.parse(PARSE_TYPE::STATEMENT);                                           \
+    REQUIRE(fp.fail() == true);                                                \
+  }
 
-TEST_CASE("Fast Parser: IF-ELSE statement test") {
-  PARSE_VALID_STATEMENT("if (\"true\") ; ")
-  PARSE_VALID_STATEMENT("if (1) { if (2) return; else return; } else return;")
-  PARSE_VALID_STATEMENT("if (1) { if (2) return; else return; }"
-                        " else { if (2) return;}")
-  PARSE_VALID_STATEMENT("if (1) if (2) ; else ;")
-  PARSE_INVALID_STATEMENT("if (\"true\")  ")
-  PARSE_INVALID_STATEMENT("if (1)  else ;")
-  PARSE_INVALID_STATEMENT("if (1) ; else ")
-  PARSE_INVALID_STATEMENT("if (1) { if (2) return; else return;  else return;")
-  PARSE_INVALID_STATEMENT("if (1 { if (2) return; else return; } else return;")
-  PARSE_INVALID_STATEMENT("if (1 { if (2) return; else return; } if return;")
-  PARSE_INVALID_STATEMENT("if (1 { else return; } else return;")
-}
+TEST_CASE("Fast Parser: simple statement test"){
+    PARSE_VALID_STATEMENT("{ 1;}")     // expression-statement
+    PARSE_VALID_STATEMENT("{ a: 1;}")  // labeled-statement
+    PARSE_VALID_STATEMENT("{ int a;}") // declaration in compound statement
+    PARSE_VALID_STATEMENT("{}") PARSE_INVALID_STATEMENT("{ 1;")
+        PARSE_INVALID_STATEMENT("{ a: 1;") PARSE_INVALID_STATEMENT("{ int a;")
+            PARSE_INVALID_STATEMENT("{ ")}
 
-TEST_CASE("Fast Parser: Jump statement") {
-  PARSE_VALID_STATEMENT("{ goto a; }")
-  PARSE_VALID_STATEMENT("{ continue; }")
-  PARSE_VALID_STATEMENT("{ break; }")
-  PARSE_VALID_STATEMENT("{ return; }")
-  PARSE_VALID_STATEMENT("{ return 1000; }")
-}
+TEST_CASE("Fast Parser: IF-ELSE statement test"){
+    PARSE_VALID_STATEMENT("if (\"true\") ; ") PARSE_VALID_STATEMENT(
+        "if (1) { if (2) return; else return; } else return;")
+        PARSE_VALID_STATEMENT(
+            "if (1) { if (2) return; else return; }"
+            " else { if (2) return;}") PARSE_VALID_STATEMENT("if (1) if (2) ; "
+                                                             "else ;")
+            PARSE_INVALID_STATEMENT("if (\"true\")  ") PARSE_INVALID_STATEMENT(
+                "if (1)  else ;") PARSE_INVALID_STATEMENT("if (1) ; else ")
+                PARSE_INVALID_STATEMENT(
+                    "if (1) { if (2) return; else return;  else return;")
+                    PARSE_INVALID_STATEMENT(
+                        "if (1 { if (2) return; else return; } else return;")
+                        PARSE_INVALID_STATEMENT(
+                            "if (1 { if (2) return; else return; } if return;")
+                            PARSE_INVALID_STATEMENT(
+                                "if (1 { else return; } else return;")}
+
+TEST_CASE("Fast Parser: Jump statement"){
+    PARSE_VALID_STATEMENT("{ goto a; }") PARSE_VALID_STATEMENT("{ continue; }")
+        PARSE_VALID_STATEMENT("{ break; }") PARSE_VALID_STATEMENT("{ return; }")
+            PARSE_VALID_STATEMENT("{ return 1000; }")}
 
 TEST_CASE("Fast Parser: loop statement test") {
   PARSE_VALID_STATEMENT("{ while (10) { 1000; } }")
