799d137f6587ff908ee2c357e897c728ee34f9da
guruhegde <s8guhegd.stud.uni-saarland.de>
2018-12-28 16:43:18 +0100

    Fix Token class
    Minor fixes
    * When all parameters for constructor has default argument value,
    it's better to define default constructor, it will be more unambiguous.

--- src/lexer/token.hpp
+++ src/lexer/token.hpp
@@ -16,34 +16,26 @@ public:
         const unsigned long column, std::string extra = "?")
       : type(type), loc(line, column), extra(std::move(extra)) {}
 
-  explicit Token(const TokenType type = TokenType::GHOST,
-                 std::string extra = "?")
-      : type(type), loc(0, 0), extra(std::move(extra)) {}
-
-  TokenType getType() const { return type; }
-  unsigned long getLine() const { return loc.getLine(); }
-  unsigned long getColumn() const { return loc.getColumn(); }
-  const std::string &getExtra() const { return extra; }
-
+  Token() : type(TokenType::GHOST), loc(0, 0), extra(std::string("?")) {}
   Token(const Token &t) = default;
   Token &operator=(const Token &t) = default;
   Token(Token &&t) = default;
   Token &operator=(Token &&t) = default;
   ~Token() = default;
-  ccc::TokenType getType() const;
 
-  unsigned long getLine() const;
-  unsigned long getColumn() const;
-  const std::string &getExtra() const;
+  TokenType getType() const { return type; }
+  unsigned long getLine() const { return loc.getLine(); }
+  unsigned long getColumn() const { return loc.getColumn(); }
+  const std::string &getExtra() const { return extra; }
   const std::string name() const;
   const std::string token_type() const;
+
   bool is_not(TokenType expected) const { return type != expected; }
   template <typename T> bool is(const T &base) const { return type == base; }
   template <typename T, typename... Args>
   bool is(const T &first, const Args &... args) const {
     return (type == first) || is(args...);
   }
-
   Precedence getPrecedence() const;
 
 private:
