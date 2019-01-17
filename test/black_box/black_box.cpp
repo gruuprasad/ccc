84bdbd749e5198ae0b3e4e0a0590a8650b348054
Lauritz Timm <s9latimm@stud.uni-saarland.de>
2019-01-17 17:33:46 +0100

    adding pretty_printer_files

--- test/black_box/black_box.cpp
+++ test/black_box/black_box.cpp
@@ -8,8 +8,46 @@ TEST_CASE("parser_success_files") {
   std::string dir = "../../parser_success_files";
   for (const auto &file : Utils::dir(&dir[0])) {
     SECTION(file) {
-      std::string flag = "--print-ast";
+      std::string flag = "--parse";
       std::string input = "../../parser_success_files/" + file;
+
+      char **ppArgs = new char *[3];
+      ppArgs[1] = &flag[0];
+      ppArgs[2] = &input[0];
+
+      if (EXIT_FAILURE == EntryPointHandler().handle(3, ppArgs))
+        FAIL("\033[1;31mUnexpected fail\033[0m");
+
+      delete[] ppArgs;
+    }
+  }
+}
+
+TEST_CASE("parser_failure_files") {
+  std::string dir = "../../parser_failure_files";
+  for (const auto &file : Utils::dir(&dir[0])) {
+    SECTION(file) {
+      std::string flag = "--parse";
+      std::string input = "../../parser_failure_files/" + file;
+
+      char **ppArgs = new char *[3];
+      ppArgs[1] = &flag[0];
+      ppArgs[2] = &input[0];
+
+      if (EXIT_SUCCESS == EntryPointHandler().handle(3, ppArgs))
+        FAIL("\033[1;31mUnexpected pass\033[0m");
+
+      delete[] ppArgs;
+    }
+  }
+}
+
+TEST_CASE("pretty_printer_files") {
+  std::string dir = "../../pretty_printer_files";
+  for (const auto &file : Utils::dir(&dir[0])) {
+    SECTION(file) {
+      std::string flag = "--print-ast";
+      std::string input = "../../pretty_printer_files/" + file;
 
       std::ifstream ifs(input);
       std::stringstream buffer;
@@ -33,25 +71,6 @@ TEST_CASE("parser_success_files") {
       std::string content = ss.str();
 
       REQUIRE_EMPTY(Utils::compare(content, expected));
-    }
-  }
-}
-
-TEST_CASE("parser_failure_files") {
-  std::string dir = "../../parser_failure_files";
-  for (const auto &file : Utils::dir(&dir[0])) {
-    SECTION(file) {
-      std::string flag = "--parse";
-      std::string input = "../../parser_failure_files/" + file;
-
-      char **ppArgs = new char *[3];
-      ppArgs[1] = &flag[0];
-      ppArgs[2] = &input[0];
-
-      if (EXIT_SUCCESS == EntryPointHandler().handle(3, ppArgs))
-        FAIL("\033[1;31mUnexpected pass\033[0m");
-
-      delete[] ppArgs;
     }
   }
 }
